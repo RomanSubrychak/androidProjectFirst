5d88723890f5ec215ff45a2b5fbb7e4c7ac6ddef
Roman Subrychak <rsubrichak@yahoo.com>
2017-06-12 18:19:01 -0500

    0.Create Project 1.Simple test created

---
+++ app/src/test/java/com/example/romansubrichak/test/MainActivityTest.java
@@ -0,0 +1,16 @@
+package com.example.romansubrichak.test;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by romansubrichak on 6/12/17.
+ */
+public class MainActivityTest {
+    @Test
+    public void onCreate() throws Exception {
+
+    }
+
+}
